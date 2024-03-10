2447db61176b13072e07abd655a85a8d7f02c11c
Daniel Bazar <daniel.bazar@gmail.com>
2024-03-10 02:35:17 +0200

    finish exercise

--- ex3.py
+++ ex3.py
@@ -1,25 +1,24 @@
+#   Daniel Bazar    314708181
+#   Lior Krengel    315850594
 import sys
 from math import log, pow, exp
 from collections import Counter
 import os
 import re
 import numpy as np
+import pandas as pd
 import time
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
 develop_txt_name = "develop.txt"
-#Getting the parameters
-#develop_txt_name = sys.argv[1]
-#test_txt_name = sys.argv[2]
-#input_word = sys.argv[3]
-#output_file_name = sys.argv[4]
+topics_file_name = 'topics.txt'
 
-# Task defintitions
+# global parameters defintitions
 MIN_WORD_APPEARANCES = 3
 NUMBER_OF_CLUSTERS = 9
 K = 10
-LAMBDA = 0.06 #From assignment 2
+LAMBDA = 0.06
 EPSILON = 0.001
 
 total_time = time.time()
@@ -27,8 +26,8 @@ def get_Data_From_File(file_name):
     ''' 
     Input: file_name the file_name we are reading from
     Output:
-        doc_words_counter, tuple contains: cluster_ID, topic_name, document_size, words_counter -> Counter
-        words_counter, tuple contains: cluster_ID, topic_name, document_size, words_counter -> Counter
+        doc_words_counter, tuple contains: cluster_ID, topics_names, document_size, words_counter -> Counter
+        words_counter, tuple contains: cluster_ID, topics_names, document_size, words_counter -> Counter
     '''
     #Each topic is separated by header line, empty line before the text and another empty line
     #So we taking only the third line in each section for the text
@@ -41,44 +40,35 @@ def get_Data_From_File(file_name):
             # Read the third line and save it
             if line_number % 4 == 1:
                 match = re.search(topic_pattern, line)
-                topic_name = match.group(1)#.split('\t')
-            if line_number % 4 == 3:
+                topics_names = match.group(1).split()
+            elif line_number % 4 == 3:
                 #We count the number of times each word appeared in this doc
-                doc_words_counter = Counter()
                 doc_words = line.strip().split()
-                for word in doc_words:
-                    if word not in doc_words_counter:
-                        doc_words_counter[word] = 0
+                doc_words_counter = Counter(doc_words)
+                words_counter.update(doc_words_counter)
 
-                    if word not in words_counter:
-                        words_counter[word] = 0
+                wt = np.zeros(NUMBER_OF_CLUSTERS)
+                wt[(topic_ID - 1) % NUMBER_OF_CLUSTERS] = 1
+                docs_tuple_list.append((wt, topics_names, len(doc_words), doc_words_counter))
+                topic_ID += 1
 
-                    words_counter[word] += 1
-                    doc_words_counter[word] += 1
-            else:
-                continue
-
-            wt = np.zeros(NUMBER_OF_CLUSTERS)
-            wt[(topic_ID - 1) % NUMBER_OF_CLUSTERS] = 1
-            # Do I need to recalculate the len?
-            docs_tuple_list.append((wt, topic_name, len(doc_words), doc_words_counter))
-            topic_ID += 1
-
-        #Removing all the words with less then X appearances from all the words
-        for word, count in list(words_counter.items()):
-            if count <= MIN_WORD_APPEARANCES:
-                del words_counter[word]
+        # Removing all the words with less then X appearances from all the words
+        words_counter = Counter({k:v for k,v in words_counter.items() if v > MIN_WORD_APPEARANCES})
 
         #Removing all the words we removed from each doc
         docs_list_filtered = []
-        for wt, topic_name, _, doc_words_counter in docs_tuple_list:
-            for word in list(doc_words_counter.keys()):
-                if word not in words_counter:
-                    del doc_words_counter[word]
-
-            docs_list_filtered.append((wt, topic_name, sum([word_cnt for word_cnt in doc_words_counter.values()]), doc_words_counter))
+        for wt, topics_names, doc_len, doc_words_counter in docs_tuple_list:
+            doc_words_counter = Counter({k:v for k,v in doc_words_counter.items() if k in words_counter})
+            doc_len = sum(doc_words_counter.values())
+            docs_list_filtered.append((wt, topics_names, doc_len, doc_words_counter))
 
     return docs_list_filtered, words_counter
+
+def get_topics(topics_file=topics_file_name):
+    with open(topics_file, 'r') as f:
+        file_lines = f.readlines()
+    topics = [t.strip() for i,t in enumerate(file_lines) if i%2==0]
+    return topics
 
 class EM:
     def __init__(self, docs, words_counter, stopping_criteria=10):
@@ -91,7 +81,7 @@ class EM:
         self.iter_scores = []
         self.stopping_criteria = stopping_criteria
     
-    def run(self):
+    def train(self):
         t = 1
         while(True):
             if (len(self.iter_scores) > 2 and self.iter_scores[-1][0] - self.iter_scores[-2][0] <= self.stopping_criteria):
@@ -107,7 +97,11 @@ class EM:
 
         self.create_Graph([likli[0] for likli in self.iter_scores], 'Log Likelihood')
         self.create_Graph([likli[1] for likli in self.iter_scores], 'Perplexity')
-        
+        self.create_confusion_matrix_and_fit()
+        accuracy = self.accuracy()
+        print('Accuracy:', accuracy)
+
+
     def create_Graph(self, values, graph_name):
         plt.plot(values)
         plt.xlabel('Iteration')
@@ -165,8 +159,38 @@ class EM:
         alpha /= alpha_sum
         self.alpha = alpha
 
+    def create_confusion_matrix_and_fit(self):
+        '''creating confusion matrix and save it as csv.
+        in addition, fitting the model with hard clustering. assiging each cluster its predicted topic'''
+        topics = get_topics()
+        # initialize confusion matrix with zeroes
+        confusion_matrix = pd.DataFrame(0, index=range(NUMBER_OF_CLUSTERS), columns=topics)
+        for doc in self.docs:
+            gold_topics = doc[1]
+            predicted_topic = np.argmax(doc[0])
+            for t in gold_topics:
+                confusion_matrix.iloc[predicted_topic][t] += 1
+
+        self.predicted_topics = confusion_matrix.idxmax(axis=1)
+        confusion_matrix['size'] = confusion_matrix.sum(axis=1)
+        confusion_matrix.sort_values(by='size', ascending=False, inplace=True)
+        confusion_matrix.to_csv('confusion_matrix.csv')
+
+    def accuracy(self):
+        '''most run the confusion matrix and fit before'''
+        correct = 0
+        topics = get_topics()
+        for doc in self.docs:
+            gold_topics = doc[1]
+            predicted_topic_idx = np.argmax(doc[0])
+            predicted_topic = self.predicted_topics[predicted_topic_idx]
+            if predicted_topic in gold_topics:
+                correct+=1
+        return correct/len(self.docs)
+
 if __name__ == '__main__':
     docs, words_counter = get_Data_From_File(develop_txt_name)
+    print('vocabulary size after filtering:', len(words_counter))
     e = EM(docs, words_counter)
-    e.run()
+    e.train()
     print(f"Total run took:{time.time() - total_time}")
